cb2751665a2cfe24080e47a61dc44bfe5b64bb10
DalalErdaini <alrudainid@gmail.com>
2022-08-14 04:37:59 +0300

    cw 6 donw

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("مرحبا بك في موقعي الذي سيحسب درجتك");
+
+left grade = prompt("اكتب درجتك بالارقام")
+
+console.log(grade);
+
+if (grade >= 90) {
+    console.log("لقد حصلت على امتياز");
+  } else if (grade >= 80) {
+    console.log("لقد حصلت على جيدا جدا "); 
+  } else if (grade >= 70) {
+    console.log("لقد حصلت على جيد");
+  } else if (grade >= 60) {
+    console.log("لقد حصلت علي مقبول");
+  } else if (grade >= 50) {
+    console.log("لقد حصلت على ضعيف");
+  } else {
+  console.log("راسب");
+
+    
+    
+}
